762c784d152df553b92807f6c5fa471f55fc69f1
Jamie  Hurd <jamiehurd@MacBook-Pro.hsd1.ut.comcast.net>
2020-01-10 13:11:03 -0700

    first commit

---
+++ assignment10/template.cpp
@@ -0,0 +1,26 @@
+/***********************************************************************
+* Program:
+*    Assignment 10, Hello World        (e.g. Assignment 10, Hello World)
+*    Brother Schwieder, CS124
+* Author:
+*    your name
+* Summary: 
+*    This program outputs the string *"Hello world!" to the console.
+*
+*    Estimated:  0.0 hrs   
+*    Actual:     0.0 hrs
+*       The most difficult part comprised remembering
+* which operator outputs the string to the console.
+************************************************************************/
+
+#include <iostream>
+
+/**********************************************************************
+ * Main initializes the program and
+ * outputs "Hello world!" to the console window.
+ ***********************************************************************/
+int main()
+{
+   std::cout << "Hello World\n";
+   return 0;
+}
