6aba2da17b0c5aac803977f9dc5c6c9bcddd513d
Jamie  Hurd <jamiehurd@MacBook-Pro.hsd1.ut.comcast.net>
2020-01-11 20:48:56 -0700

    assignment12.cpp improved

--- assignment12/assignment12/assignment12.cpp
+++ assignment12/assignment12/assignment12.cpp
@@ -1,9 +1,28 @@
-//
-//  assignment12.cpp
-//  assignment12
-//
-//  Created by Jamie  Hurd on 1/11/20.
-//  Copyright © 2020 Jamie  Hurd. All rights reserved.
-//
+/***********************************************************************
+* Program:
+*    Assignment 12, Input and Variables
+*    Brother Schwieder, CS124
+* Author:
+*    Jamie Hurd
+* Summary:
+*    This program provides practice for outputting user input.
+*
+*    Estimated:  0.0 hrs
+*    Actual:     0.0 hrs
+*       The most difficult part comprised analyzing
+* why we use float for this instead of double.
+************************************************************************/
 
+#include <iostream>
+#include <iomanip>
 #include <stdio.h>
+
+int main()
+{
+    std::cout << std::fixed;
+    std::cout.precision(2);
+    float income = 0.0;
+    std::cout << "\tYour monthly income: ";
+    std::cin >> income;
+    std::cout << "Your income is: $" << std::setw(9) << income << std::endl;
+}
