fd08ced146370433dce71dee889b231c67c30ae7
Jamie  Hurd <jamiehurd@MacBook-Pro.hsd1.ut.comcast.net>
2020-01-15 21:42:03 -0700

    project02b passing first test not second

--- project02b/project02b/project02b.cpp
+++ project02b/project02b/project02b.cpp
@@ -11,8 +11,9 @@
 *
 *    Estimated:  0.0 hrs
 *    Actual:     0.0 hrs
-*       The most difficult part comprised analyzing
-* why we use float for this instead of double.
+*       The most difficult part comprised trying
+* to do more than what was required because
+* I then had to go back and redo this project.
 ************************************************************************/
 #include <iostream>
 #include <iomanip>
@@ -23,26 +24,6 @@
 * user input and outputting the monthly budget
 * for said user according to formatting requirements.
  *****************************************/
-/**This program keeps track of your monthly budget
-Please enter the following:
-        Your monthly income: 1000.00
-        Your budgeted living expenses: 650.00
-        Your actual living expenses: 700.00
-        Your actual taxes withheld: 100.00
-        Your actual tithe offerings: 120.00
-        Your actual other expenses: 150.00
-
-The following is a report on your monthly expenses
-        Item                  Budget          Actual
-        =============== =============== ===============
-        Income          $    1000.00    $    1000.00
-        Taxes           $       0.00    $     100.00
-        Tithing         $       0.00    $     120.00
-        Living          $     650.00    $     700.00
-        Other           $       0.00    $     150.00
-        =============== =============== ===============
-        Difference      $       0.00    $       0.00
-*/
 int main()
 {
     std::cout << std::fixed;
@@ -62,31 +43,31 @@ int main()
     actTithe = 0.0;
     std::cout << "This program keeps track of your monthly budget" << std::endl;
     std::cout << "Please enter the following:" << std::endl;
-    std::cout << "\tYour monthly income:  ";
+    std::cout << "\tYour monthly income: ";
     std::cin >> income;
-    std::cout << "\tYour budgeted living expenses:  ";
+    std::cout << "\tYour budgeted living expenses: ";
     std::cin >> budgetExp;
-    std::cout << "\tYour actual living expenses:  ";
+    std::cout << "\tYour actual living expenses: ";
     std::cin >> actLiving;
-    std::cout << "\tYour actual taxes withheld:  ";
+    std::cout << "\tYour actual taxes withheld: ";
     std::cin >> actTaxes;
-    std::cout << "\tYour actual tithe offerings:  ";
+    std::cout << "\tYour actual tithe offerings: ";
     std::cin >> actTithe;
-    std::cout << "\tYour actual other expenses:  ";
+    std::cout << "\tYour actual other expenses: ";
     std::cin >> actOther;
     std::cout << std::endl;
     float differenceInit = 0.00;
     //difference = income - (livingExp + taxes + tithe + other);
     //differenceTwo = actIncome - (actLiving + actTaxes + actTithe + actOther);
     std::cout << "The following is a report on your monthly expenses" << std::endl;
-    std::cout << "Item" << std::setw(23) << "Budget" << std::setw(18) << "Actual" << std::endl;
-    std::cout << "===============" <<  "  " << "===============" << "  " <<  "===============" << std::endl;
-    std::cout << "Income" << std::setw(12) << "$" << std::setw(4) << "" << income << std::setw(6) << "$" << std::setw(10) << income << std::endl;
-    std::cout << "Taxes" << std::setw(13) << "$" << std::setw(7) << "" << taxes << std::setw(6) << "$" << std::setw(10) << actTaxes << std::endl;
-    std::cout << "Tithing" << std::setw(11) << "$" << std::setw(7) << "" << tithe << std::setw(6) << "$" << std::setw(10) << actTithe << std::endl;
-    std::cout << "Living" << std::setw(12) << "$" << std::setw(7) << "" << livingExp << std::setw(6) << "$" << std::setw(10) << actLiving << std::endl;
-    std::cout << "Other" << std::setw(13) << "$" << std::setw(7) << "" << other << std::setw(6) << "$" << std::setw(10) << actOther << std::endl;
-    std::cout << "===============" <<  "  " << "===============" << "  " <<  "===============" << std::endl;
-    std::cout << "Difference" << std::setw(8) << "$" << std::setw(9) << "" << differenceInit << std::setw(4) << "$" << std::setw(10) << differenceInit << std::endl ;
+    std::cout << "\tItem" << std::setw(24) << "Budget" << std::setw(16) << "Actual" << std::endl;
+    std::cout << "\t===============" <<  " " << "===============" <<  " ===============" << std::endl;
+    std::cout << "\tIncome" << std::setw(11) << "$" << std::setw(7) << "" << income << std::setw(5) << "$" << std::setw(11) << income << std::endl;
+    std::cout << "\tTaxes" << std::setw(12) << "$" << std::setw(7) << "" << taxes << std::setw(5) << "$" << std::setw(11) << actTaxes << std::endl;
+    std::cout << "\tTithing" << std::setw(10) << "$" << std::setw(7) << "" << tithe << std::setw(5) << "$" << std::setw(11) << actTithe << std::endl;
+    std::cout << "\tLiving" << std::setw(11) << "$" << std::setw(7) << "" << budgetExp << std::setw(5) << "$" << std::setw(11) << actLiving << std::endl;
+    std::cout << "\tOther" << std::setw(12) << "$" << std::setw(7) << "" << other << std::setw(5) << "$" << std::setw(11) << actOther << std::endl;
+    std::cout << "\t===============" <<  " " << "===============" << " " <<  "===============" << std::endl;
+    std::cout << "\tDifference" << std::setw(7) << "$" << std::setw(7) << "" << differenceInit << std::setw(5) << "$" << std::setw(11) << differenceInit << std::endl ;
     return 0;
 }
