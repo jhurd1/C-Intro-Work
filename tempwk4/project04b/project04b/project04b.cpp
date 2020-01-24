9f680c2a63016ad9598adc3e404817f333632f7c
Jamie  Hurd <jamiehurd@MacBook-Pro.hsd1.ut.comcast.net>
2020-01-23 18:30:32 -0700

    Variables don't need to be all local; they just don't work with an extemporaneous call to parent class object

--- tempwk4/project04b/project04b/project04b.cpp
+++ tempwk4/project04b/project04b/project04b.cpp
@@ -10,13 +10,18 @@
 * for formulating my monthly budget.
 *
 *    Estimated:  1.0 hrs
-*    Actual:     1.5 hrs
+*    Actual:     3.0 hrs
 *       The most difficult part comprised getting
 * the tax calculator to recognize the income variable. I
 * assumed that calling getIncome() would allow it to see
 * it, but it didn't. Showing the method as part of the class
 * allowed me to scope "income" in by itself without having
-* to call extemporaneous methods, like getters.
+* to call extemporaneous methods, like getters. Conversely,
+* I realized that instantiating a new object of the class before
+* calling a getter on it did not work, either. Only calling the
+* getter directly (without a class object fastened to it) or
+* placing the variable itself allowed other methods to get access
+* to these variables or data members.
 ************************************************************************/
 #include <stdio.h>
 #include <iomanip>
@@ -51,6 +56,8 @@ private:
      double actTithe;
      double actLiving;
      double actOther;
+      //double tithe;
+      double differenceTwo;
    public:
   
 /*********************************************************************
@@ -93,153 +100,144 @@ double getBracket6()
 {
    return bracket6;
 }
-
-double getTaxes()
-{
-   return taxes;
-}
-
-double getTithe()
-{
-   return tithing;
-}
-
-double getOther()
-{
-   return other;
-}
-
-void prompt()
-{
-   std::cout << "\tYour monthly income: ";
-   std::cin >> income;
-   std::cout << "\tYour budgeted living expenses: ";
-   std::cin >> living;
-   std::cout << "\tYour actual living expenses: ";
-   std::cin >> actLiving;
-   std::cout << "\tYour actual taxes withheld: ";
-   std::cin >> actTaxes;
-   std::cout << "\tYour actual tithe offerings: ";
-   std::cin >> actTithe;
-   std::cout << "\tYour actual other expenses: ";
-   std::cin >> actOther;
-   std::cout << std::endl;
-}
-
-   
-double getIncome(){
-   return income;
-}
-
-double getLiving(){
-   return living;
-}
-
-double getActIncome()
-{
-   return actIncome;
-}
-
-double getActTaxes(){
-   return actTaxes;
-}
-
-double getActTithe(){
-   return actTithe;
-}
-
-double getActLiving(){
-   return actLiving;
-}
-
-double getActOther(){
-   return actOther;
-}
-
 /*********************************************************************
  * Compute the user's tax bracket for projecting the tax imposition.
  *********************************************************************/
 double computeTax(){
    BudgetCT calc;
-   annual = income * 12;
-   if((annual > 0) && (annual < 15100))
-   {
-      //calc.getBracket1();
-      taxes = bracket1 * income;
-   }
-   if((annual > 15100) && (annual < 61300))
-   {
-     //calc.getBracket2();
-      taxes = bracket2 * income;
-   }
-   if((annual > 61300) && (annual < 123700))
-   {
-      //calc.getBracket3();
-      taxes = bracket3 * income;
-   }
-   if((annual > 123700) && (annual < 188450))
-   {
-       //calc.getBracket4();
-       taxes = bracket4 * income;
-   }
-   if((annual > 188450) && (annual < 336550))
-   {
-      //calc.getBracket5();
-      taxes = bracket5 * income;
-   }
-   if(annual > 336550){
-      //calc.getBracket6();
-      taxes = bracket6 * income;
-   }
+       annual = income * 12;
+       if((annual > 0) && (annual < 15100))
+       {
+          //calc.getBracket1();
+          taxes = getBracket1() * income;
+       }
+       if((annual > 15100) && (annual < 61300))
+       {
+         //calc.getBracket2();
+          taxes = getBracket2() * income;
+       }
+       if((annual > 61300) && (annual < 123700))
+       {
+          //calc.getBracket3();
+          taxes = getBracket3() * income;
+       }
+       if((annual > 123700) && (annual < 188450))
+       {
+           //calc.getBracket4();
+           taxes = getBracket4() * income;
+       }
+       if((annual > 188450) && (annual < 336550))
+       {
+          //calc.getBracket5();
+          taxes = bracket5 * income;
+       }
+       if(annual > 336550){
+          //calc.getBracket6();
+          taxes = getBracket6() * income;
+       }
    return taxes;
    }
+      
+      /******************************************
+      * Accessors and mutators help provide programmers with
+      * control over what parameters can be implemented
+      * with child classes or using the parent class.
+       *****************************************/
+      //mutators
+      double setTithing(double tithing){
+         this->tithing = tithing;
+         tithing = income * .1;
+         return tithing;
+      }
+      
+      double setOther(double other){
+         this->other = other;// I had line 155 assigned here, jumping the gun
+         other = income - (taxes + tithing + living);//I needed to tell the compiler that this other should be other first on line 154
+         return other;//Had to do the same for setTithing.
+      }
+      //accessors
+      double getIncome(){
+         std::cout << "\tYour monthly income: ";
+         std::cin >> income;
+         return income;
+      }
+      
+      double getLiving(){
+         std::cout << "\tYour budgeted living expenses: ";
+         std::cin >> living;
+         return living;
+      }
+      
+      double getActLiving(){
+         std::cout << "\tYour actual living expenses: ";
+         std::cin >> actLiving;
+         return actLiving;
+      }
+      
+      double getOther(){
+         std::cin >> other;
+         return other;
+      }
+      
+      double getActOther(){
+         std::cout << "\tYour actual other expenses: ";
+         std::cin >> actOther;
+         std::cout << std::endl;
+         return actOther;
+      }
+      
+      double getActTithe(){
+         std::cout << "\tYour actual tithe offerings: ";
+         std::cin >> actTithe;
+         return actTithe;
+      }
+      
+      double getActTax(){
+         std::cout << "\tYour actual taxes withheld: ";
+         std::cin >> actTaxes;
+         return actTaxes;
+      }
 
-/*********************************************************************
-* Calculate the tithing.
-*********************************************************************/
-double calcTithing()
-{
-   BudgetCT forTithe;
-   forTithe.getIncome();
-   tithing = income * .1;
-   return tithing;
-}
+       void displayIntro(){
+               std::cout << "This program keeps track of your monthly budget" << std::endl;
+               std::cout << "Please enter the following:" << std::endl;
+       }
+       
+       void displayReport(){
+          BudgetCT budget;
+         double differenceInit = 0.00;
+         difference = income - (actLiving + actTaxes + actTithe + actOther);
+         std::cout << "The following is a report on your monthly expenses" << std::endl;
+         std::cout << "\tItem" << std::setw(24) << "Budget" << std::setw(16) << "Actual" << std::endl;
+         std::cout << "\t===============" <<  " " << "===============" <<  " ===============" << std::endl;
+         std::cout << "\tIncome          $" << std::setw(11) << income <<  "    $" << std::setw(11) << income << std::endl;
+         std::cout << "\tTaxes           $" << std::setw(11) << computeTax()  << "    $" << std::setw(11) << actTaxes << std::endl;
+         std::cout << "\tTithing         $" << std::setw(11) << setTithing(tithing) << "    $"  << std::setw(11) << actTithe << std::endl;
+         std::cout << "\tLiving          $" << std::setw(11) << living << "    $"  << std::setw(11) << actLiving << std::endl;
+         std::cout << "\tOther           $" << std::setw(11) << setOther(other) <<  "    $"  << std::setw(11) << actOther << std::endl;
+         std::cout << "\t===============" <<  " " << "===============" << " " <<  "===============" << std::endl;
+         std::cout << "\tDifference" << std::setw(7) << "$" << std::setw(7) << "" << differenceInit << std::setw(5) << "$" << std::setw(11) << difference << std::endl ;
+       }
+   };
 
-/*********************************************************************
-* Display the doubleroduction for the report.
-*********************************************************************/
-void displayIntro(){
-      BudgetCT dis;
-      std::cout << "This program keeps track of your monthly budget" << std::endl;
-      std::cout << "Please enter the following:" << std::endl;
-      dis.prompt();
+   /******************************************
+   *Main accesses public members from class Budget
+   * before spinning the program up by tying them together.
+   * Getters and setters allow programmers to protect data
+   * members by making variables private.
+    *****************************************/
+   int main()
+   {
+      std::cout << std::fixed;
+      std::cout.precision(2);
+      BudgetCT temp;
+      temp.displayIntro();
+      temp.getIncome();
+      temp.getLiving();
+      temp.getActLiving();
+      temp.getActTax();
+      temp.getActTithe();
+      temp.getActOther();
+      temp.displayReport();
+      return 0;
    }
-    
-void displayReport(){
-   std::cout << std::fixed;
-   std::cout.precision(2);
-   BudgetCT budget;
-   double differenceInit = 0.00;
-   taxes = 0.0;
-   difference = income - (actLiving + actTaxes + actTithe + actOther);
-   std::cout << "The following is a report on your monthly expenses" << std::endl;
-   std::cout << "\tItem" << std::setw(24) << "Budget" << std::setw(16) << "Actual" << std::endl;
-   std::cout << "\t===============" <<  " " << "===============" <<  " ===============" << std::endl;
-   std::cout << "\tIncome          $" << std::setw(11) << income <<  "    $" << std::setw(11) << income << std::endl;
-   std::cout << "\tTaxes           $" << std::setw(11) << budget.computeTax()  << "    $" << std::setw(11) << actTaxes << std::endl;
-   std::cout << "\tTithing         $" << std::setw(11) << budget.calcTithing() << "    $"  << std::setw(11) << actTithe << std::endl;
-   std::cout << "\tLiving          $" << std::setw(11) << living << "    $"  << std::setw(11) << actLiving << std::endl;
-   std::cout << "\tOther           $" << std::setw(11) << actOther <<  "    $"  << std::setw(11) << actOther << std::endl;
-   std::cout << "\t===============" <<  " " << "===============" << " " <<  "===============" << std::endl;
-   std::cout << "\tDifference" << std::setw(7) << "$" << std::setw(7) << "" << differenceInit << std::setw(5) << "$" << std::setw(11) << difference << std::endl ;
-}
-};
-/*********************************************************************
-* Spin up the program with main.
-*********************************************************************/
-int main()
-{
-   BudgetCT begin;
-   begin.displayIntro();
-   begin.displayReport();
-}
