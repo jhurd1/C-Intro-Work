c73ae7539891de165dc003a6def1806a37b34084
Jamie  Hurd <jamiehurd@MacBook-Pro.hsd1.ut.comcast.net>
2020-01-28 21:02:51 -0700

    project05 initialized but need calendar display and date number logic

--- tempwk5/assignment21/assignment21/assignment21.cpp
+++ tempwk5/assignment21/assignment21/assignment21.cpp
@@ -1,9 +1,106 @@
-//
-//  assignment21.cpp
-//  assignment21
-//
-//  Created by Jamie  Hurd on 1/25/20.
-//  Copyright © 2020 Jamie  Hurd. All rights reserved.
-//
-
+/***********************************************************************
+* Program:
+*    Assignment 21, Debugging
+*    Brother Schwieder, CS124
+* Author:
+*    Jamie Hurd
+* Summary:
+*    This program contains the signatures or stub methods for the program outlined.
+*
+*    Estimated:  1.0 hrs
+*    Actual:     .25 hrs
+*       The most difficult part comprised not having more to do with it yet.
+************************************************************************/
 #include <stdio.h>
+#include <iomanip>
+#include <iostream>
+/**********************************************************************
+* Debug holds data members.
+*********************************************************************/
+class Debug{
+private:
+   double devalue;
+   double periodCost;
+   double usageCost;
+   double mileage;
+   double gas;
+   double parking;
+   double repairs;
+   double tires;
+   double insurance;
+protected:
+public:
+/**********************************************************************
+* Constructors
+*********************************************************************/
+   Debug(){
+      
+   }
+   
+   Debug(double devalue){
+      
+   }
+/**********************************************************************
+* Accessors and Mutators
+*********************************************************************/
+   double getPeriodCost(){
+      promptParking();
+      promptGas();
+      promptRepairs();
+      promptTires();
+      promptInsurance();
+      periodCost = parking + gas + repairs + tires + insurance;
+      return periodCost;
+   }
+   
+   double getUsageCost(){
+      promptDevalue();
+      promptMileage(mileage);
+      usageCost = devalue + mileage;
+      return usageCost;
+   }
+   
+/**********************************************************************
+* Class methods
+*********************************************************************/
+   void promptDevalue(){
+      
+   }
+   
+   void promptInsurance(){
+      
+   }
+   
+   void promptParking(){
+      
+   }
+   
+   double promptMileage(double mileage){
+      return mileage;
+   }
+   
+   void promptGas(){
+      
+   }
+   
+   void promptRepairs(){
+      
+   }
+   
+   void promptTires(){
+      
+   }
+   
+   void display(){
+      std::cout << "Success\n";
+   }
+};
+
+/**********************************************************************
+* Main calls data members.
+*********************************************************************/
+int main()
+{
+   Debug debug;
+   debug.display();
+}
---
+++ tempwk5/project05/project05/project05.cpp
@@ -0,0 +1,153 @@
+/***********************************************************************
+* Program:
+*    Project 05, Calendar
+*    Brother Schwieder, CS124
+* Author:
+*    Jamie Hurd
+* Summary:
+*    This program incorporates a calendar, which
+* calculates the days of the month for a year  after 1752.
+*
+*    Estimated:  1.0 hrs
+*    Actual:     1.5 hrs
+*       The most difficult part comprised
+************************************************************************/
+
+/*I can think of a couple of ways to display the days. I could display the days as strings and
+ create a different calendar configuration for each of seven years (focusing on the ones
+ digit, beginning with three since 1753 is the first year) and for February on leap years
+ . I could use math and output the days by returning one of 31 integers for each place
+ and using a generic calendar format using variables to reference each of 31 dates;
+ calculating the dates using an offset for each leap year and each of seven different
+ ones digits in the years.
+*/
+#include <stdio.h>
+#include <iomanip>
+#include <iostream>
+class Calendar{
+private:
+   int month;
+   int year;
+   bool isLeapYear;
+   int offset;
+protected:
+public:
+/***********************************************************************
+ *Constructor. These are useful when doing actual OOP; when you need to instantiate a new
+ *object of this class to call data members out of scope.
+ ***********************************************************************/
+   Calendar()
+   {
+      
+   }
+/***********************************************************************
+*Non-default Constructor
+***********************************************************************/
+   Calendar(int month, int year)
+   {
+      
+   }
+/***********************************************************************
+*Accessors
+***********************************************************************/
+   int getMonth()
+   {
+      return month;
+   }
+   
+   int getYear()
+   {
+      return year;
+   }
+   
+   bool getLeapYear()
+   {
+      return isLeapYear;
+   }
+   
+   int getOffSet()
+   {
+      return offset;
+   }
+/***********************************************************************
+*Mutators
+***********************************************************************/
+   void setMonth(int month){
+      this->month = month;//may need to change the sequence of this line in the body of this code
+   }
+   
+   void setYear(int year)
+   {
+      this->year = year;
+   }
+   
+   void setLeapYear(bool isLeapYear)
+   {
+      this->isLeapYear = isLeapYear;
+   }
+   
+   void setOffSet(int offset)
+   {
+      offset = 1;
+      this->offset = offset;
+   }
+/***********************************************************************
+*Calculate Leap Years
+***********************************************************************/
+   int leapYear()
+   {
+      //if the year is divisible by four and 400, the year is a leap year.
+      if((year % 4 == 0) && (year % 400 == 0))
+      {
+         isLeapYear = true;
+      }
+      if(isLeapYear == true)
+      {
+         
+      }
+      return 0;
+   }
+   
+/***********************************************************************
+*Prompt
+***********************************************************************/
+   void prompt()
+   {
+      std::cout << "Enter a month number: " << std::endl;
+      std::cin >> month;
+      std::cout << "Enter year: " << std::endl;
+      std::cin >> year;
+   }
+
+/***********************************************************************
+*CheckInput comprises great practice for passing in data between functions.
+***********************************************************************/
+int checkInput()
+   {
+      Calendar checker;
+      if((month > 12) || (year < 1753))
+      {
+         std::cin.fail();
+         std::cin.clear();
+         std::cin.ignore();
+         checker.prompt();
+      }
+      return 0;
+   }
+   
+/***********************************************************************
+*Display
+***********************************************************************/
+   void display()
+   {
+      std::cout << "\tSu  Mo  Tu  We  Th  Fr  Sa" << std::endl;
+   }
+};
+
+int main()
+{
+   Calendar temp;
+   temp.prompt();
+   temp.checkInput();
+   temp.display();
+}
