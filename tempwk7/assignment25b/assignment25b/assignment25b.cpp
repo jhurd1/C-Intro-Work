18935aa24da55c38f889bc69f0110d66db256681
Jamie  Hurd <jamiehurd@MacBook-Pro.hsd1.ut.comcast.net>
2020-02-07 21:00:25 -0700

    25b is very close but the loop in the template won't work as is

--- tempwk7/assignment25b/assignment25b/assignment25b.cpp
+++ tempwk7/assignment25b/assignment25b/assignment25b.cpp
@@ -20,8 +20,8 @@
 class LoopDesign
 {
 private:
-   int width = 2;
-   int otherWidth = 4;
+   int otherWidth = 3;
+   int testWidth = 4;
    char separator = ' ';
    int numDays = 1;
    int offset;
@@ -35,13 +35,13 @@ public:
    template<typename T> void printElement(T t, const char& width)
    {
       //make this formatted line reflect a calendar line
-      std::cout << t << std::setw(otherWidth) << "Su"
-      << std::setw(otherWidth) << "Mo"
-      << std::setw(otherWidth) << "Tu"
-      << std::setw(otherWidth) << "We"
-      << std::setw(otherWidth) << "Th"
-      << std::setw(otherWidth)<< "Fr"
-      << std::setw(otherWidth) << "Sa"
+      std::cout << t << std::setw(testWidth) << "Su"
+      << std::setw(testWidth) << "Mo"
+      << std::setw(testWidth) << "Tu"
+      << std::setw(testWidth) << "We"
+      << std::setw(testWidth) << "Th"
+      << std::setw(testWidth)<< "Fr"
+      << std::setw(testWidth) << "Sa"
       << std::endl;
    }
 
@@ -50,7 +50,11 @@ public:
 *******************************************************************/
 template<typename T> void printNext(T t, const char& width)
    {
-      std::cout << t << std::setw(otherWidth) << numDays++;// increments numDays from 1-31
+      for(int i = 0; i <= 6; i++)
+      {
+           std::cout << t << std::setw(otherWidth) << numDays++;// increments numDays from 1-31
+      }
+      std::cout << std::endl;
    }
 
 /*******************************************************************
@@ -58,18 +62,25 @@ template<typename T> void printNext(T t, const char& width)
 *******************************************************************/
    int displayTable(int numDays, int offset)
    {
-      offset = 6;
-      printElement(' ', width);
-      if(offset == 6)
-      {
-      for(int i = 1; i <= 31; i++)
+      numDays = 1;
+      printElement(' ', otherWidth);
+      //for(int i = 1; i <= 31; i++)
+      //{
+         while(numDays > 0 && numDays < 5)
          {
-                printNext(' ', width);
+            printNext(' ', otherWidth);
+            numDays++;
+           // break;
          }
+         /*while(numDays >= 6 && numDays < 13)
+         {
+            printNext(' ', width);
+            numDays++;
+            break;
+         }*/
+      //}
+        return 0;
       }
-      // Implement the offset here, later: if(offset == )
-      return 0;
-   }
 };
 
 /******************************************************************
