c8e8f5388b6bb110b268e409d303faaae7fc84b5
Jamie  Hurd <jamiehurd3@gmail.com>
2020-02-28 21:59:05 -0700

    33 pointer madness

--- tempwk9/assign33/assign33/assign33.cpp
+++ tempwk9/assign33/assign33/assign33.cpp
@@ -19,13 +19,23 @@
 /************************************************************************
 *prompt to fill the accounts
 ************************************************************************/
-float prompt(float sam, float sue)//account needs to be here?
+float promptSue()//account needs to be here?
 {
-   std::cout << "What is Sam's balance?  ";
-   std::cin >> sam;
+   float sue;
    std::cout << "What is Sue's balance?  ";
    std::cin >> sue;
    return sue;
+}
+
+/************************************************************************
+*prompt to fill the accounts
+************************************************************************/
+float promptSam()//account needs to be here?
+{
+   float sam;
+   std::cout << "What is Sam's balance?  ";
+   std::cin >> sam;
+   return sam;
 }
 
 /************************************************************************
@@ -39,43 +49,49 @@ void displayHeader()
 /************************************************************************
 *calculate the cost
 ************************************************************************/
-float calculate(float *pAccount, float account, float sam, float sue, float dinner, float movie, float iceCream)
+float calculate(float *pAccount, float sam, float sue, float dinner, float movie, float iceCream)
 {
-   float total;
-   pAccount = &account;//store account's address in the account pointer
+   float account;
+   pAccount = &account;//float account;//to meet this challenge, account needs to go here for total
+   //but the problem is, how do you do math with a pointer against sam and sue which are plain floats?
    if(sue > sam)
      {
-        account = sue;
-        total = (dinner + movie + iceCream) - sue;
+        std::cout << "Sam's balance: " <<  "$" << sam << std::endl;
+        account = (dinner + movie + iceCream) - sue;
+        std::cout << "Sue's balance: " <<  "$" << account << std::endl;
      } else
      {
-        account = sam;
-        total = (dinner + movie + iceCream) - sam;
+        //account = sam;
+        account = (dinner + movie + iceCream) - sam;
+        std::cout << "Sam's balance: " <<  "$" << account << std::endl;
+        std::cout << "Sue's balance: " <<  "$" << sue << std::endl;
      }
-   return total;
+   return account;
 }
 
+/************************************************************************
+*calculate the cost
+************************************************************************/
 float balance(float sam, float sue)
 {
-   std::cout << "Sam's balance: " <<  "$" << sam << std::endl;
-   std::cout << "Sue's balance: " <<  "$" << sue << std::endl;
+   
    return 0;
 }
 /************************************************************************
 *Reduce the value of the debit card according to the cost of the date
 ************************************************************************/
-void date(float *pAccount, float account, float sam, float sue, float dinner, float movie, float iceCream)//Here we declare the pAccount as a master copy or "weblink" of a resume
-//using the address-of symbol
+float date()
 {
-   pAccount = &account;
+   float dinner;
+   float movie;
+   float iceCream;
    std::cout << "\tDinner:" << std::setw(5) << " ";
    std::cin >> dinner;
    std::cout << "\tMovie:" << std::setw(5) << " ";
    std::cin >> movie;
    std::cout << "\tIce Cream:" << std::setw(5) << " ";
    std::cin >> iceCream;
-   //calculate(pAccount, account, sam, sue, dinner, movie, iceCream);
-   balance(sam, sue);
+   return 0;
 }
 
 /************************************************************************
@@ -83,8 +99,11 @@ void date(float *pAccount, float account, float sam, float sue, float dinner, fl
 ************************************************************************/
 int main()
 {
-   float sam, sue, account, dinner, movie, iceCream, *pAccount;
-   prompt(sam, sue);
+   float account, dinner, movie, iceCream;
+   float *pAccount = &account;
+   float sam = promptSam();
+   float sue = promptSue();
    displayHeader();
-   date(pAccount, account, sam, sue, dinner, movie, iceCream);
+   float temp = date();
+   float calc = calculate(&account, sam, sue, dinner, movie, iceCream);
 }
