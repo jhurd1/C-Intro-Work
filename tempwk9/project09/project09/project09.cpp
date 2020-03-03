0d3e95f61477945bb7118f6b0e7da3d6b66a85b6
Jamie  Hurd <jamiehurd3@gmail.com>
2020-03-02 19:02:09 -0700

    project07 end of file causing infinite loop

--- tempwk9/project09/project09/project09.cpp
+++ tempwk9/project09/project09/project09.cpp
@@ -16,24 +16,63 @@
 #include <iomanip>
 #include <iostream>
 #include <cstring>
+#include <fstream>
 /************************************************************************
-*main()
+*bool playAgain()
 ************************************************************************/
-
-
-/************************************************************************
-*main()
-************************************************************************/
-
-/************************************************************************
-*readFile
-************************************************************************/
-char* readFile(std::string fileName)
+bool playAgain()
 {
-   int i = 0;
+   bool play = false;
+   return play;
+}
+
+/************************************************************************
+*askQuestions
+************************************************************************/
+char* askQuestion(char aos[10][256])
+{
+   return 0;
+}
+/************************************************************************
+*getFilelName && displayHeader; this only needs to get the file name, not read it, before passing
+* it on to readFile.
+************************************************************************/
+std::string getFileName()//test with cout; working
+{
+   std::string fileName;
+   std::cout << "Please enter the filename of the Mad Lib: ";
+   std::cin >> fileName;
+   return fileName;
+}
+/************************************************************************
+*readFile; error check 1024 chars, 32 lines, 80 chars in each line, 256 words, and 32-char length
+* words
+************************************************************************/
+std::string readFile(std::string fileName)
+{
+   /*int i = 0;
    char j = ' ';
-   char *aos[10][256];
-   return aos[i][j];
+   char *aos[10][256];*/
+   std::ifstream fin(fileName);
+   //std::cin.ignore(std::numeric_limits<std::streamsize>::max());//ignore was faulting the data flow here;
+      //perhaps call ignore after getline(), if need be
+   fin.clear();
+   std::getline(std::cin, fileName);
+   fin.seekg(0, std::ios_base::beg);//presumably counts the bytes or chars in a file; may need eof method to help
+   while(!fin.eof())//why is this an infinite loop? It's supposed to stop at file's end!
+   {
+      if(!fin.fail())
+        {
+           int count = 0;
+                  count++;
+           std::cout << "Count: " << std::endl;
+        } else
+        {
+           std::cout << "You done failed.";
+             break;
+        }
+   }
+   return fileName;
 }
 /************************************************************************
 *displayStory
@@ -51,24 +90,14 @@ std::string displayStory(char aos[10][256])//memory misallocation happening here
    std::cout << "\tAnother animal: " << std::endl;
    return 0;
 }
-/************************************************************************
-*displayHeader
-************************************************************************/
-std::string getFileName()//test with cout; working
-{
-   std::string fileName;
-   std::cout << "Please enter the filename of the Mad Lib: " << std::endl;
-   std::cin.ignore();
-   std::getline(std::cin, fileName);
-   return fileName;
-}
 
 /************************************************************************
 *main()
 ************************************************************************/
 int main()
 {
+   std::string fileName;
    //char aos[10][256];
-   getFileName();
-   //displayStory(aos);
+   fileName = getFileName();
+   readFile(fileName);
 }
