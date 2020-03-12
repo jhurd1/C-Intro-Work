e2cafea9617d8e733318e9315c1d58aa70fd3cb9
Jamie  Hurd <jamiehurd3@gmail.com>
2020-03-11 18:47:11 -0600

    10 is displaying the story but without spaces

--- tempwk10/project10/project10/project10.cpp
+++ tempwk10/project10/project10/project10.cpp
@@ -20,19 +20,17 @@
 /************************************************************************
 *bool playAgain()
 ************************************************************************/
-bool playAgain(bool play)
+bool playAgain()
 {
-   char temp;
-   do
-   {
+   char answer;
    std::cout << "Do you want to play again (y/n)?";
-   std::cin >> temp;
-   if(temp == 'n' || (temp != 'y' && temp != 'n'))
+   std::cin >> answer;
+   answer = tolower(answer);
+   if(answer == 'n')
    {
-      play = false;
-   }
-}while (temp == 'y');
-   return play;
+      return false;
+   } else
+   return true;
 }
 
 /************************************************************************
@@ -45,7 +43,7 @@ void askQuestion(char word[])
    std::cout << "\t" << (char)(toupper(word[1]));//char casts toupper which converts to pure ASCII value
    for(int i = 2; word[i]; i++)
    {
-      if(word[i] != '"' || word[i] != '.' || word[i] != ',' || word[i] != (char)13)//carriage return is char 13; cast is used
+     if(word[i] == '_')
       {
          std::cout << " ";
       } else
@@ -54,38 +52,44 @@ void askQuestion(char word[])
       }
    }
    std::cout << ": ";
-   std::cin.ignore();
    std::cin.getline(word, 32);
 }
 /************************************************************************
 *getFilelName && displayHeader; this only needs to get the file name, not read it, before passing
 * it on to readFile.
 ************************************************************************/
-void getFileName(char fileName[])//test with cout; working
+void getFileName(char fileName[])//tested with cout; working
 {
    std::cout << "Please enter the filename of the Mad Lib: ";
    std::cin >> fileName;
+   std::cin.ignore(256, '\n');
 }
 
 /************************************************************************
 *displayStory
 * isolate readFile first, then come back to this
+  if(word[i] == '"' || word[i] == '.' || word[i] == ',' || word[i] == (char)13)
 ************************************************************************/
-void displayStory(char word[])
+void displayStory(char fileName[256], char story[][32], char word[256])
 {
-   //std::cout << "Thank you for playing.\n";
-   for(int i = 0; i <= *word; i++)
+   std::ifstream fin(fileName);//need line break and whitespace between words
+   int i = 0;
+   while(fin >> word)
    {
+      if(word[i] == '"' || word[i] == '.' || word[i] == ',' || word[i] == (char)13 || isspace(word[i]))
+      {
+         std::cout << " ";
+      }
       std::cout << word;
    }
-      std::cout << "Thank you for playing.\n";
+   std::cout << "Thank you for playing.\n";
 }
 
 /************************************************************************
 *readFile; error check 1024 chars, 32 lines, 80 chars in each line, 256 words, and 32-char length
 * words.
 ************************************************************************/
-int readFile(char fileName[], char story[][32])//isolate this function first; it's not working
+int readFile(char fileName[], char story[][32])
 {
    int count = 0;
    std::ifstream fin(fileName);
@@ -107,8 +111,7 @@ int readFile(char fileName[], char story[][32])//isolate this function first; it
 int main()
 {
    bool play = true;
-   char word[256];//This is going to get passed into other functions not expecting it here
-   //Evaluate whether this entails single words because you need the story for display, not a word.
+   char word[256];
    while(play)
    {
       char fileName[256];
@@ -116,7 +119,7 @@ int main()
       getFileName(fileName);
       /*int numWords =*/
       readFile(fileName, story);
-      displayStory(word);
-      playAgain(play);
+      displayStory(fileName, story, word);
+      play = playAgain();
    }
 }
