c782dbf3063281f6f43d145d8ab1a9d9ee949401
Jamie  Hurd <jamiehurd3@gmail.com>
2020-03-14 09:16:22 -0600

    remove space method recommended

--- tempwk10/project10/project10/project10.cpp
+++ tempwk10/project10/project10/project10.cpp
@@ -18,6 +18,46 @@
 #include <fstream>
 
 /************************************************************************
+*trim: citation, tiptip.ca/2010/03/trim-leading-or-trailing-white-spaces.html
+ ///https://www.youtube.com/watch?v=W_defvKTiVc
+************************************************************************/
+void removeASpace(char * p, char c)
+{
+   if(NULL == p)
+   {
+      return;
+   }
+      char * pDest = p;
+      while(*p)
+      {
+         if(*p != c)
+         {
+            *pDest++ = *p;
+            p++;
+         }
+         *pDest = '\0';
+      }
+   }
+
+/************************************************************************
+*bool flag()
+************************************************************************/
+void flag(int count, char temp[])
+{
+   count = 0;
+   for(int i = 0; i < count; i++)
+   {
+      if(count)//first index
+      {
+         std::cout << "";//take out the first space on the line
+      } else
+      {
+         std::cout << " " << temp;//add a space between words thereafter
+      }
+   }
+}
+
+/************************************************************************
 *bool playAgain()
 ************************************************************************/
 bool playAgain()
@@ -39,14 +79,14 @@ bool playAgain()
 * The only conditions when a space is not inserted is when the preceding character is an open
 * quote or a newline, or when the following character is a closed quote, period, or comma.
 ************************************************************************/
-void askQuestion(char word[])
+void askQuestion(char word[])//This function, remember, outputs the list of replacement words and its prompts
 {
       std::cout << "\t" << (char)(toupper(word[1]));//char casts toupper which converts to pure ASCII value
    for(int i = 2; word[i]; i++)
    {
      if(word[i] == '_')
       {
-         std::cout << " ";
+         std::cout << " "; //This is not the culprit of the extra space preceding the first char, either.
       } else
       {
          std::cout << (char)(tolower(word[i]));
@@ -61,7 +101,7 @@ void askQuestion(char word[])
 ************************************************************************/
 void getFileName(char fileName[])//tested with cout; working
 {
-   std::cout << "Please enter the filename of the Mad Lib: ";
+   std::cout << "Please enter the filename of the Mad Lib: ";//This space didn't cause the extra space ont he line below.
    std::cin >> fileName;
    std::cin.ignore(256, '\n');
 }
@@ -69,7 +109,7 @@ void getFileName(char fileName[])//tested with cout; working
 /************************************************************************
 *Manage the tokens.
 ************************************************************************/
-void tokenManager(char antecedent[], char temp[], char next[])//don't think I need antecedent or next here
+void tokenManager(char antecedent[], char temp[], char next[])
 {
    switch(temp[1])
    {
@@ -77,7 +117,7 @@ void tokenManager(char antecedent[], char temp[], char next[])//don't think I ne
          std::cout << std::endl;
          break;
       case '<':
-         std::cout << ' ' << '"';
+         std::cout << ' ' << '"';//This is not causing the anamalous space on the first line preceding the first char
          break;
       case '>':
          std::cout << '"';
@@ -94,18 +134,12 @@ void tokenManager(char antecedent[], char temp[], char next[])//don't think I ne
 /************************************************************************
 *Manage spacing
 ************************************************************************/
-void spaceManager(char antecedent[], char temp[], char next[])
+void spaceManager(int count, char antecedent[], char temp[], char next[])//represents the first three words
 {
-   int i = 0;
-   if(antecedent[i] == 'a' || antecedent[i] == 'A')
-   {
-      std::cout << " ";
-   }
+   //std::cout << antecedent[1] << "antecedent [1]"; prints the second letter of the first word
+   //flag(count); flag isn't doing anything here
    switch(antecedent[1])
    {
-      case 0://target the first element of the array
-         std::cout << "";//it worked!
-         break;
       case '!':
          std::cout << temp;
          //std::cout << "\n";
@@ -117,13 +151,19 @@ void spaceManager(char antecedent[], char temp[], char next[])
          std::cout << temp;
          break;
       case '.':
-         std::cout << " "  << temp;
+         //std::cout << " "  << temp;
+         flag(count, temp);
          break;
       case ',':
-         std::cout << " " << temp;
+         //std::cout << " " << temp;
+         flag(count, temp);
          break;
+         //What happens if you insert a new case where there is no space, for the very first word?
+      
+         //What happens if you change this default to case ' '? // it inserts an entirely new line and no text displays
       default:
-            std::cout << " " << temp;
+         //flag(count, temp);
+            std::cout << " " << temp;//Removing the default case only leaves a new line on the first line from somewhere and stops printing every subsequent word
          break;
    }
 }
@@ -133,15 +173,23 @@ void spaceManager(char antecedent[], char temp[], char next[])
 ************************************************************************/
 void displayStory(int count, char story[][32])
 {
-   std::cout << "\n";
+   std::cout << "\n";//removing this caused the program to time out
    for(int i = 0; i < count; i++)//count is the story array's size
    {
-      if(story[i][0] == ':')
+      if(story[i][0] == ':')//change the second index from 0 to i-- Doing so prints all the words on the first line rather than line breaking.
+         //Change the first index of the first array from i to i + 1; doing so skips the line break and prints everything on the first line again
       {
-         tokenManager(story[i - 1], story[i], story[i + 1]);
+         //flag(count, temp); did nothing
+         //Changing the last parameter from i + 1 to 1 did not resolve the extra space at the start of the lines. What about i - 1? i - 1 didn't do anything that I can tell here.
+         tokenManager(story[1], story[i], story[i + 1]);//Changing this first parameter from i - 1 to 1, appears to have separated that pesky article 'a' from the first word
+         //std::cout << story[i + 1] << "This is the test."; //This prints after the first word at the first space.
+         //std::cout << story[1] << "TEST [1]"; This prints the first word and when it does now the subsequent lines are left-aligned correctly but not the very first one.
+         //std::cout << story[i - 1] << "TEST [i - 1"; This prints the last word on the line
       } else
       {
-         spaceManager(story[i - 1], story[i], story[i + 1]);
+         //flag(count, temp); did nothing
+         //what about changing the last parameter here from i + 1 to 1? Did nothing. Try i - 1 for third parameter now. Did nothing.
+         spaceManager(count, story[1], story[i], story[i + 1]);//Changing the first parameter from i - 1 to 1 put the uppercase back on the first char, but didn't resolve the extra space preceding it
       }
 }
     std::cout << "\n";
