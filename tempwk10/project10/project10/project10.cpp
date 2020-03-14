66fc05891530e032b72af5578e6c154544cc3cc0
Jamie  Hurd <jamiehurd3@gmail.com>
2020-03-14 17:35:46 -0600

    displayStory revised

--- tempwk10/project10/project10/project10.cpp
+++ tempwk10/project10/project10/project10.cpp
@@ -18,46 +18,6 @@
 #include <fstream>
 
 /************************************************************************
-*trim: citation, tiptip.ca/2010/03/trim-leading-or-trailing-white-spaces.html
- ///https://www.youtube.com/watch?v=W_defvKTiVc
-************************************************************************/
-void removeASpace(char * p, char c)
-{
-   if(NULL == p)
-   {
-      return;
-   }
-      char * pDest = p;
-      while(*p)
-      {
-         if(*p != c)
-         {
-            *pDest++ = *p;
-            p++;
-         }
-         *pDest = '\0';
-      }
-   }
-
-/************************************************************************
-*bool flag()
-************************************************************************/
-void flag(int count, char temp[])
-{
-   count = 0;
-   for(int i = 0; i < count; i++)
-   {
-      if(count)//first index
-      {
-         std::cout << "";//take out the first space on the line
-      } else
-      {
-         std::cout << " " << temp;//add a space between words thereafter
-      }
-   }
-}
-
-/************************************************************************
 *bool playAgain()
 ************************************************************************/
 bool playAgain()
@@ -136,8 +96,6 @@ void tokenManager(char antecedent[], char temp[], char next[])
 ************************************************************************/
 void spaceManager(int count, char antecedent[], char temp[], char next[])//represents the first three words
 {
-   //std::cout << antecedent[1] << "antecedent [1]"; prints the second letter of the first word
-   //flag(count); flag isn't doing anything here
    switch(antecedent[1])
    {
       case '!':
@@ -151,16 +109,11 @@ void spaceManager(int count, char antecedent[], char temp[], char next[])//repre
          std::cout << temp;
          break;
       case '.':
-         //std::cout << " "  << temp;
-         flag(count, temp);
+         std::cout << " "  << temp;
          break;
       case ',':
-         //std::cout << " " << temp;
-         flag(count, temp);
+         std::cout << " " << temp;
          break;
-         //What happens if you insert a new case where there is no space, for the very first word?
-      
-         //What happens if you change this default to case ' '? // it inserts an entirely new line and no text displays
       default:
          //flag(count, temp);
             std::cout << " " << temp;//Removing the default case only leaves a new line on the first line from somewhere and stops printing every subsequent word
@@ -176,23 +129,54 @@ void displayStory(int count, char story[][32])
    std::cout << "\n";//removing this caused the program to time out
    for(int i = 0; i < count; i++)//count is the story array's size
    {
-      if(story[i][0] == ':')//change the second index from 0 to i-- Doing so prints all the words on the first line rather than line breaking.
-         //Change the first index of the first array from i to i + 1; doing so skips the line break and prints everything on the first line again
-      {
-         //flag(count, temp); did nothing
-         //Changing the last parameter from i + 1 to 1 did not resolve the extra space at the start of the lines. What about i - 1? i - 1 didn't do anything that I can tell here.
-         tokenManager(story[1], story[i], story[i + 1]);//Changing this first parameter from i - 1 to 1, appears to have separated that pesky article 'a' from the first word
-         //std::cout << story[i + 1] << "This is the test."; //This prints after the first word at the first space.
-         //std::cout << story[1] << "TEST [1]"; This prints the first word and when it does now the subsequent lines are left-aligned correctly but not the very first one.
-         //std::cout << story[i - 1] << "TEST [i - 1"; This prints the last word on the line
-      } else
-      {
-         //flag(count, temp); did nothing
-         //what about changing the last parameter here from i + 1 to 1? Did nothing. Try i - 1 for third parameter now. Did nothing.
-         spaceManager(count, story[1], story[i], story[i + 1]);//Changing the first parameter from i - 1 to 1 put the uppercase back on the first char, but didn't resolve the extra space preceding it
-      }
-}
-    std::cout << "\n";
+       if (i == 0) {
+           std::cout << story[i]; //print first word without space
+       } else {
+           if(story[i][0] == ':') { //TOKEN
+               switch(story[i][1])
+               {
+                  case '!':
+                     std::cout << std::endl;
+                     break;
+                  case '<':
+                     std::cout << " " << '"';
+                     break;
+                  case '>':
+                     std::cout << '"';
+                     break;
+                  case '.':
+                     std::cout << '.';
+                     break;
+                  case ',':
+                     std::cout << ',';
+                     break;
+               }
+           } else { //WORD
+               if(story[i-1][0] == ':') { //PRIOR WORD WAS TOKEN
+                   switch(story[i-1][1])
+                   {
+                      case '!':
+                         std::cout << story[i];
+                         break;
+                      case '<':
+                         std::cout << story[i];
+                         break;
+                      case '>':
+                         std::cout << " " << story[i];
+                         break;
+                      case '.':
+                         std::cout << " " << story[i];
+                         break;
+                      case ',':
+                         std::cout << " " << story[i];
+                         break;
+                   }
+               } else { //PRIOR WORD WAS WORD
+                   std::cout << " " << story[i]; //default space before word
+               }
+           }
+       }
+   }
 }
 
 /************************************************************************
